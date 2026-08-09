06f56fcf4c9ae8bf8e9bb52f5c9a5d0a03950a85
Ahmed.Ezzat <ahmedezzateasa@gmail.com>
2026-08-09 18:56:19 +0300

    Add DRIVE_ROOT_FOLDER_ID config for Google Drive document storage

--- be/config.py
+++ be/config.py
@@ -8,11 +8,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # Path to the downloaded Google Service Account JSON key file (Sheets access)
+    # Path to the downloaded Google Service Account JSON key file (Sheets + Drive access)
     GOOGLE_CREDENTIALS_FILE = os.getenv("GOOGLE_CREDENTIALS_FILE", "credentials.json")
 
     # The ID of your Google Sheet (found in its URL between /d/ and /edit)
     SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "")
+
+    # The ID of the Google Drive folder (in your own Drive) that the service
+    # account has been shared on as Editor. All employee document sub-folders
+    # are created inside this root folder. Found in the folder's URL after /folders/.
+    DRIVE_ROOT_FOLDER_ID = os.getenv("DRIVE_ROOT_FOLDER_ID", "")
 
     # Secret key used to sign our own session tokens (issued after Google verifies the user)
     SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret-in-production")
